8f6a83e72a19f9f3e444017bf73dbf188a0202af
abdulahmedov <abdulahmedov@gmail.com>
2024-01-28 23:41:58 +0300

    fix .editorconfig

---
+++ app/chatbot.py
@@ -0,0 +1,16 @@
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import LlamaCpp
+
+
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+llm = LlamaCpp(
+    model_path="phi-2.Q4_K_M.gguf",
+    temperature=0.75,
+    max_tokens=2000,
+    top_p=1,
+    callback_manager=callback_manager,
+    verbose=True,  # Verbose is required to pass to the callback manager
+)
