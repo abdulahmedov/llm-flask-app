293a4d2456f0fbea724e6976e0c906c3a224d43f
abdulahmedov <abdulahmedov@gmail.com>
2024-02-02 14:38:26 +0300

    add some fixes to tha app

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,17 +1,9 @@
-from typing import Dict, List
+from typing import Dict, List, Any
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import LlamaCpp
-
-
-class DummyLlamaCppForTest(LlamaCpp):
-    def __init__(self) -> None:
-        pass
-    
-    def invoke(self, prompt: str) -> str:
-        return prompt
     
 
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
@@ -33,131 +25,19 @@ class Prompt(str):
         self.body = f'Question: {body}'
  
 
-# class Intent(str):
-#     def __init__(self, prompt: Prompt) -> None:
-#         pass
-
-
-# class IntentClassfier:
-#     def __init__(self) -> None:
-#         pass
-
-#     def classify(self, intent: Prompt) -> Skill:
-#         return Skill
-    
-#     def calculate_levenstein(self, string):
-
-
-
-
-# class PromptEngineer:
-#     def __init__(self) -> None:
-#         pass
-
-#     def classify_intent() -> None:
-#         pass
-    
-#     def build_prompt(self, intent: Intent) -> Prompt:
-#         return
-
-
-# class Skill:
-#     def __init__(self, name: str, question: str) -> None:
-#         self.name = name
-#         self.question = question
-    
-#     def ask_query(self):
-#         return Response(self.question)
-    
-#     def process_query(self, query: str, llm: LlamaCpp) -> Response:
-#         pass 
-    
-#     def exit(self, arg):
-#         pass
-
-#     def __repr__(self):
-#         pass
-
-#     def __str__(self):
-#         pass
-
-
-# class SkillSelector:
-#     def __init__(self, model, skills: List[Skill], classify_method: str) -> None:
-#         self.model = model
-#         self.skills = skills
-
-#     def select_skill(self, intent: Prompt):
-#         return self.skills
-
-
-
-# from dataclasses import dataclass
-
-
-# @dataclass
-# class State:
-#     val: str = 'init'
-
-
-# class DialogueFlow:
-#     def __init__(self, current: State) -> None:
-#         self.current = current
-#         self.previous = None
-
-
-# class ChatBot:
-#     def __init__(self, skill_selector: SkillSelector) -> None:
-#         self.skill_selector = skill_selector
-#         self.state = State()
-#         self.dialogue_flow = DialogueFlow(self.state)
-    
-#     def run():
-#         if self.state == 'init':
-#             return Response('Hi, I am AI powered bot')
-        
-#         elif self.state
-    
-#     def process_intent(self, intent: Intent):
-#         self.state, self.skill = self.skill_selector(intent)
-#         self.skills[self.state].ask
-
-#     def response(self, Prompt):
-#         if self.state == 'init':
-#             pass
-#         elif self.state == 'skill':
-#             pass
-
-#     def move_back(self) -> None:
-#         self.dialogue_flow.current = self.dialogue_flow.previous
-    
-#     def move_forward(self, skill: Skill) -> None:
-#         self.dialogue_flow.previous = self.dialogue_flow.current
-#         self.dialogue_flow.current = skill.name
-
-    
-#     def step_back(self):
-#         pass
-
-#     def reset(self):
-#         pass
-
-#     def joke(self):
-#         pass
-
-
 def chatbot_response(prompt: Prompt, llm: LlamaCpp) -> str:
     response = llm.invoke(prompt).replace('\n', '<br>').replace('    ', '&nbsp;'*4)
     return response
 
 
-def is_toxic(prompt: Prompt, toxic_words_reference: List[str], threshold: float) -> bool:
-    toxicity = 0
+def is_toxic(prompt: Prompt, toxic_words_reference: Any, threshold: float) -> bool:
+    toxic_words_reference = [str(x) for x in toxic_words_reference]
+    toxicity = 0.0
     words = prompt.body
     words = words.replace(',', '').replace('.', '')
-    words = words.split(' ')
-    for word in words:
-        word = word
+    # words = words
+    for word in words.split(' '):
+        word = word.lower()
         if word in toxic_words_reference:
             toxicity += 1 / len(words)
             if toxicity >= threshold:
