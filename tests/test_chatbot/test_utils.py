e9394c9f80950053f56a1a5d4378541b737830e1
abdulahmedov <abdulahmedov@gmail.com>
2024-02-03 15:11:06 +0300

    fix tests

--- tests/test_chatbot/test_utils.py
+++ tests/test_chatbot/test_utils.py
@@ -71,34 +71,3 @@ def test_is_prompt_toxic(prompt_string: str, toxic_words_reference: List[str], t
 )
 def test_is_chatbot_response_valid(prompt_string: Prompt, llm: LlamaCpp, expected_result: str):
     assert chatbot_response(prompt_string, llm) == expected_result
-
-
-def encode(input_string):
-    if not input_string:
-        return []
-    count = 1
-    prev = ""
-    lst = []
-    for character in input_string:
-        if character != prev:
-            if prev:
-                entry = (prev, count)
-                lst.append(entry)
-            count = 1
-            prev = character
-        else:
-            count += 1
-    entry = (character, count)
-    lst.append(entry)
-    return lst
-
-
-def decode(lst):
-    q = ""
-    for character, count in lst:
-        q += character * count
-    return q
-
-@given(text())
-def test_decode_inverts_encode(prompt_string: str):
-    assert decode(encode(prompt_string)) == prompt_string
